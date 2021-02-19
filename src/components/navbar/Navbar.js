cf0d379b670b2189d07251311b4e7b22f7181dd8
Samuel Kinuthia <skinuthia77@gmail.com>
2021-02-19 15:11:25 +0300

    Icon modified

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -30,7 +30,7 @@ const Navbar = () => {
         <nav className="navbar">
             <div className="navbar-container">
                 <Link to='/' className='navbar-logo' onClick={closeMobileMenu}>
-                    RCTApp <i className="fab fa-typo3"/>
+                    RCTApp <i className="fa fa-compass" aria-hidden="true"/>
                 </Link>
                 <div className='menu-icon' onClick={handleClick}>
                     <i className={click ? 'fas fa-times' : 'fas fa-bars'}/>
