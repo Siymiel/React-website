b986f0012f53dfe6bf4fa15dc2989328eebca38e
Samuel Kinuthia <skinuthia77@gmail.com>
2021-02-19 14:21:26 +0300

    First Commit

---
+++ src/components/pages/Home.js
@@ -0,0 +1,16 @@
+import Herosection from '../herosection/Herosection';
+import '../../App.css';
+import Cards from '../cards/Cards';
+import Footer from '../footer/Footer';
+
+const Home = () => {
+    return (
+        <>
+            <Herosection/>
+            <Cards/>
+            <Footer/>
+        </>
+    )
+}
+
+export default Home
